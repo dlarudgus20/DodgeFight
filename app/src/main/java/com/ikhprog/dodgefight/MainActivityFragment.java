d4dc8ac282feee43ba68cbf75bca368343fff901
dlarudgus20 <dlarudgus20@naver.com>
2015-06-30 21:07:47 +0900

    Initial commit

---
+++ app/src/main/java/com/ikhprog/dodgefight/MainActivityFragment.java
@@ -0,0 +1,20 @@
+package com.ikhprog.dodgefight;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.View;
+
+public class MainActivityFragment extends Fragment
+{
+    public MainActivityFragment()
+    {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
